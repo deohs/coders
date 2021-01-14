79d62a14324e1fe7d812291ba411421cca20c59d
Brian High <high@uw.edu>
2021-01-14 12:59:50 -0800

    Cleaned up code for best practices

--- demos/data_import/sph_core_faculty_pubmed/sph_core_faculty.R
+++ demos/data_import/sph_core_faculty_pubmed/sph_core_faculty.R
@@ -1,7 +1,16 @@
-# Get a list of UW SPH Core Faculty from the school's website
+# Find articles in Pubmed authored by UW SPH Core Faculty
 
-library(rvest)
+# Load packages, installing as needed
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(rvest, readr, dplyr, purrr, easyPubMed)
 
+# Setup data folder
+data_dir <- "data"
+if(!dir.exists(data_dir)) {
+  dir.create(data_dir, showWarnings = FALSE, recursive = TRUE)
+}
+
+# Get a list of core faculty from the UW SPH website
 url <- 'https://sph.washington.edu/faculty/sphcore'
 doc <- read_html(url)
 nodes <- doc %>% html_nodes("div.faculty-tile-text-wrap")
@@ -9,29 +18,14 @@ names <- nodes %>% html_node("h3") %>% html_text()
 titles <- doc %>% html_nodes("p.fac-title") %>% html_text()
 degrees <- doc %>% html_nodes("p.fac-deg") %>% html_text()
 df <- tibble(fac_name = names, fac_title = titles, fac_degree = degrees)
-df
 
-
-# Get PubMed articles for each faculty member.
-
-library(readr)
-library(dplyr)
-library(purrr)
-library(easyPubMed)
-
-# Define function to retrieve results from a Pubmed search
+# Define function to retrieve results from a Pubmed author search
 get_articles <- function(aut) {
   # Retrieve PubMed data and return a list of articles
   my_query <- paste0(aut, "[Author]")
   my_query <- get_pubmed_ids(pubmed_query_string = my_query)
   my_data <- fetch_pubmed_data(my_query, encoding = "ASCII")
   articles_to_list(my_data)
-}
-
-# Setup data folder
-data_dir <- "data"
-if(!dir.exists(data_dir)) {
-  dir.create(data_dir, showWarnings = FALSE, recursive = TRUE)
 }
 
 # Query Pubmed for articles with SPH core faculty members as authors
