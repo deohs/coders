d6c80817b3f18827f83c7233ad29424b8cb60121
Brian High <high@uw.edu>
2021-01-19 18:07:42 -0800

    Cleaned up comments

--- demos/data_import/text_file_encoding/read_qinteractive_files_simple.R
+++ demos/data_import/text_file_encoding/read_qinteractive_files_simple.R
@@ -9,17 +9,17 @@ read.qint <- function(fn, num.rows = 2) {
   # Set column name patterns to extract
   col.names <- c('Raw score', 'Scaled score', 'Completion Time')
   
-  # Search for the column name patterns and store their row numbers
+  # Search for the column names and store the row numbers for their data values
   row.nums <- lapply(col.names, function(pattern) {
     seq(grep(pattern, df$V3)[1] + 1, length.out = num.rows)
   })
   
-  # Extract the values from the row numbers desired above
+  # Extract the values from the row numbers and store as columns in a dataframe
   scores.lst <- lapply(row.nums, function(row.nums) as.numeric(df$V3[row.nums]))
   scores.df <- as.data.frame(do.call("cbind", scores.lst))
   names(scores.df) <- col.names
   
-  # Combine the values with the filename and subtest names
+  # Combine the dataframe with new columns for the filename and subtest names
   Filename <- rep(basename(fn), num.rows)
   Subtest <- c('Matrix Reasoning', 'Digit Span')
   cbind(Subtest, scores.df, Filename)
