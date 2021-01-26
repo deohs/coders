c1a3fa05dca0eda282ddc1396a1347b64e665203
Brian High <high@uw.edu>
2021-01-26 14:52:43 -0800

    Switched to 30-min avg, updated captions.

--- demos/data_import/epa_aqi/pa_multi_station_plot_current_seattle.R
+++ demos/data_import/epa_aqi/pa_multi_station_plot_current_seattle.R
@@ -14,16 +14,17 @@ if (!dir.exists(data_dir)) dir.create(data_dir, showWarnings = FALSE)
 csv_filename <- file.path(data_dir, paste0('pa_seattle_', today(), '.csv'))
 
 # Create a URL to fetch all stations within a rectangular area (Seattle)
+# Field "pm_5" is the 30-min average PM2.5 AQI
 base_url <- 'https://www.purpleair.com/data.json'
 query_list <- list(fetch = "true", 
                    nwlat = "47.71803233",
                    selat = "47.47633042",
                    nwlng = "-122.49252319",
                    selng = "-122.24670410",
-                   fields = "pm_1")
+                   fields = "pm_5")
 
 # Get station data
-timestamp <- now()
+timestamp <- now(tzone = Sys.timezone())
 json_txt <- content(GET(base_url, query = query_list), "text")
 pa_fields <- fromJSON(json_txt)[['fields']]
 pa_df <- data.frame(fromJSON(json_txt)[['data']])
@@ -34,7 +35,7 @@ write.csv(df, csv_filename, row.names = FALSE)
 
 # Prepare data for plotting
 df <- df %>%
-  select(PM2.5 = "pm_1", conf, Lat, Lon, Flags) %>%
+  select(PM2.5 = "pm_5", conf, Lat, Lon, Flags) %>%
   mutate(across(everything(), ~ as.numeric(as.character(.)))) %>%
   filter(Flags == 0, PM2.5 < 1000, conf > 50) %>%
   select(-c(conf,-Flags)) %>%
@@ -66,12 +67,15 @@ gg <- gg+ geom_point(data = df, aes(x = Lon, y = Lat, fill = AQI),
                      colour = "black", pch = 21, size = 2, alpha = 0.5)
 gg <- gg + scale_fill_manual(values = aqi_colors, drop = FALSE)
 gg <- gg + scale_alpha(guide = FALSE)
-gg <- gg + labs(x = NULL, y = NULL, fill = "PM2.5 AQI", 
+gg <- gg + labs(x = NULL, y = NULL, 
                 title = "Seattle Air Quality Index", 
-                subtitle = paste0("Purple Air (", timestamp, ")"))
+                subtitle = "US EPA PM2.5 AQI 30-min avg",
+                caption = paste("Source: Purple Air", 
+                                format(timestamp, "%Y-%m-%d %H:%M %Z")))
 gg <- gg + theme_map(base_family = "Helvetica")
 gg <- gg + theme(plot.title = element_text(face = "bold"))
-gg <- gg + theme(legend.position = "right")
+gg <- gg + theme(legend.title = element_blank())
+gg <- gg + theme(legend.position = "right", plot.caption.position = "plot")
 gg <- gg + theme(strip.background = element_rect(fill = "white", color = "white"))
 gg <- gg + theme(strip.text = element_text(face = "bold", hjust = 0))
 gg
@@ -80,8 +84,9 @@ gg
 img_dir <- "img"
 if (!dir.exists(img_dir)) dir.create(img_dir, showWarnings = FALSE)
 png_filename <- file.path(img_dir, gsub('\\.csv', '.png', basename(csv_filename)))
-ggsave(png_filename, width = 2.5, height = 3)
+ggsave(png_filename, width = 2.5, height = 2.5)
 
 # Resize this image using ImageMagick "covert" from Bash for use in presentation
 resize_png_filename <- gsub('\\.png', '_50pct_resize.png', png_filename)
 system2('convert', args = c('-resize "50%"', png_filename, resize_png_filename))
+
