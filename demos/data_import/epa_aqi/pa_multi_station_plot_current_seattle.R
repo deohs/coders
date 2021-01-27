0fe7fcbf38318ad66a2d3030b080431bbdfd4f33
Brian High <high@uw.edu>
2021-01-26 17:45:54 -0800

    Used con2aqi(), changed to 10-min avg.

--- demos/data_import/epa_aqi/pa_multi_station_plot_current_seattle.R
+++ demos/data_import/epa_aqi/pa_multi_station_plot_current_seattle.R
@@ -5,7 +5,7 @@
 if (! suppressPackageStartupMessages(require(pacman))) {
   install.packages('pacman', repos = 'http://cran.us.r-project.org')
 }
-pacman::p_load(readr, tidyr, dplyr, ggplot2, ggthemes, ggmap, scales, 
+pacman::p_load(readr, tidyr, dplyr, ggplot2, ggthemes, ggmap, scales, con2aqi,
                RColorBrewer, grid, gridExtra, lubridate, httr, jsonlite)
 
 # Get current PM2.5 AQI from PurpleAir
@@ -14,14 +14,12 @@ if (!dir.exists(data_dir)) dir.create(data_dir, showWarnings = FALSE)
 csv_filename <- file.path(data_dir, paste0('pa_seattle_', today(), '.csv'))
 
 # Create a URL to fetch all stations within a rectangular area (Seattle)
-# Field "pm_5" is the 30-min average PM2.5 AQI
 base_url <- 'https://www.purpleair.com/data.json'
 query_list <- list(fetch = "true", 
                    nwlat = 47.72,
                    selat = 47.51,
                    nwlng = -122.43,
-                   selng = -122.25,
-                   fields = "pm_5")
+                   selng = -122.25)
 
 # Get station data
 json_txt <- content(GET(base_url, query = query_list), "text")
@@ -34,16 +32,18 @@ df <- pa_df %>% rename(pa_id = ID) %>%
 write.csv(df, csv_filename, row.names = FALSE)
 
 # Prepare data for plotting
+# Field "pm_1" is the 10-min average PM2.5 AQI
 df <- df %>%
-  select(PM2.5 = "pm_5", conf, Lat, Lon, Flags) %>%
+  select(PM2.5 = "pm_1", conf, Lat, Lon, Flags) %>%
   mutate(across(everything(), ~ as.numeric(as.character(.)))) %>%
   filter(Flags == 0, PM2.5 < 1000, conf > 50) %>%
   select(-c(conf,-Flags)) %>%
-  select(PM2.5, Lat, Lon)
+  select(PM2.5, Lat, Lon) %>% 
+  mutate(AQI = con2aqi("pm25", PM2.5))
 
 # Create a factor variable for AQI with levels used in the EPA color scale
 # See: https://www.airnow.gov/aqi/aqi-basics/
-df$AQI <- cut(df$PM2.5, ordered_result = TRUE, include.lowest = TRUE, 
+df$AQI <- cut(df$AQI, ordered_result = TRUE, include.lowest = TRUE, 
               breaks = c(0, 50, 100, 150, 200, 300, Inf), 
               labels = c("0-50", "50-100", "100-150", 
                          "150-200", "200-300", "300+"))
@@ -61,12 +61,12 @@ basemap <- get_stamenmap(bbox, zoom = 11, maptype = "toner-lite")
 # Create the map
 gg <- ggmap(basemap)
 gg <- gg+ geom_point(data = df, aes(x = Lon, y = Lat, fill = AQI), 
-                     colour = "black", pch = 21, size = 2, alpha = 0.5)
+                     colour = "gray", pch = 21, size = 1.5, alpha = 0.75)
 gg <- gg + scale_fill_manual(values = aqi_colors, drop = FALSE)
 gg <- gg + scale_alpha(guide = FALSE)
 gg <- gg + labs(x = NULL, y = NULL, 
                 title = "Seattle Air Quality Index", 
-                subtitle = "US EPA PM2.5 AQI 30-min. avg.",
+                subtitle = "US EPA PM2.5 AQI 10-min. avg.",
                 caption = paste("Source: Purple Air", 
                                 format(timestamp, "%Y-%m-%d %H:%M:%S %Z")))
 gg <- gg + theme_map(base_family = "Helvetica")
