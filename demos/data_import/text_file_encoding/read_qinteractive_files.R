ba36b7e57e00057c6db70827948ebf61e4af08a6
Brian High <high@uw.edu>
2021-02-02 11:26:27 -0800

    Code clean-up; better problem description.

--- demos/data_import/text_file_encoding/read_qinteractive_files.R
+++ demos/data_import/text_file_encoding/read_qinteractive_files.R
@@ -1,18 +1,25 @@
-# Read Q-Interactive files and combine results. Only read sections containing
-# the columns: Raw score, Scaled score, or Completion Time (seconds), and only 
-# read the first section of these if there are more than one present.
+# Unruly Data Example: CSV file with atypical character encoding and sections.
+#
+# Read Q-Interactive files and combine results. Only read sections RAW SCORES, 
+# SCALED SCORES and SUBTEST COMPLETION TIMES, and only read the first instance 
+# of each section if there are more than one present. Get the first and last 
+# columns for the first two rows of each section and combine into a single 
+# "wide" format dataframe. Include the filename as an additional column in 
+# order to track which rows came from which files. The column names will be 
+# Subtest, Raw score, Scaled score, Completion Time (seconds) and filename.
 
 library(tidyr)
 library(dplyr)
 library(stringr)
 
+sections <- c('RAW SCORES', 'SCALED SCORES', 'SUBTEST COMPLETION TIMES')
+
 df <- list.files('data', pattern = "\\.csv$", recursive = T, full.names = T) %>%
     lapply(function (fn) {
       lines <- scan(fn, "raw", fileEncoding = "UTF-16LE", sep = '\n', quiet = T)
-      col_names <- c('Raw score', 'Scaled score', 'Completion Time (seconds)')
-      row_nums <- sapply(col_names, function(x) str_which(lines, fixed(x))[1])
+      row_nums <- sapply(sections, function(x) str_which(lines, fixed(x))[1])
       lapply(row_nums, function(n) { 
-        read.csv(text = lines[n:(n + 2)], na.strings = c("null", "-"), 
+        read.csv(text = lines[n + 1:3], na.strings = c("null", "-"), 
                  check.names = F) %>% select(1, 3) %>% pivot_longer(-Subtest)
       }) %>% bind_rows() %>% pivot_wider() %>% mutate(filename = basename(fn))
     }) %>% bind_rows()
