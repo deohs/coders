60b501c5e791d5d51d7f121535b32d9577a4e52e
Brian High <high@uw.edu>
2021-02-01 17:56:50 -0800

    Corrected script to avoid warnings.

--- demos/data_import/text_file_encoding/read_qinteractive_files.R
+++ demos/data_import/text_file_encoding/read_qinteractive_files.R
@@ -2,26 +2,29 @@
 # the columns: Raw score, Scaled score, or Completion Time (seconds), and only 
 # read the first section of these if there are more than one present.
 
-library(readr)
 library(tidyr)
 library(dplyr)
+library(stringr)
 
-df <- 
-  list.files('data', pattern = "\\.csv$", recursive = T, full.names = T) %>%
+df <- list.files('data', pattern = "\\.csv$", recursive = T, full.names = T) %>%
     lapply(function (fn) {
       lines <- scan(fn, "raw", fileEncoding = "UTF-16LE", sep = '\n', quiet = T)
-      nums <- grep('Raw score|Scaled score|Completion Time', lines)[1:3]
-      lapply(nums, function(x) {
-        read_csv(lines[x:(x + 2)]) %>% select(1, 3) %>% 
+      col_names <- c('Raw score', 'Scaled score', 'Completion Time (seconds)')
+      row_nums <- sapply(col_names, function(x) str_which(lines, fixed(x))[1])
+      lapply(row_nums, function(n) { 
+        read.csv(text = lines[n:(n + 2)], na.strings = c("null", "-"), 
+                 check.names = F) %>% select(1, 3) %>% 
           pivot_longer(-Subtest) %>% mutate(value = as.numeric(value))
-    }) %>% bind_rows() %>% pivot_wider() %>% mutate(filename = basename(fn))
-  }) %>% bind_rows()
+      }) %>% bind_rows() %>% pivot_wider() %>% mutate(filename = basename(fn))
+    }) %>% bind_rows()
+
+df
 
 # Expected Output
 #
 # A tibble: 4 x 5
 # Subtest          `Raw score` `Scaled score` `Completion Time (seconds)` filename       
-# <chr>                  <dbl>          <dbl>                       <dbl> <chr>          
+# <fct>                  <dbl>          <dbl>                       <dbl> <chr>          
 # 1 Matrix Reasoning        12              7                        243. 0001_scores.csv
 # 2 Digit Span              18              8                         NA  0001_scores.csv
 # 3 Matrix Reasoning        18             NA                        239. 0002_scores.csv
