883c40db4624a730bbd644c8a9c22eab803f9439
Brian High <high@uw.edu>
2021-03-16 18:41:03 -0700

    Minor code cleanup.

--- demos/models/chisqr_by_group.R
+++ demos/models/chisqr_by_group.R
@@ -10,13 +10,9 @@ library(titanic)
 titanic_train %>% 
   group_by(Pclass, Survived, Sex) %>%
   summarize(freq = n(), .groups = "drop") %>%
-  group_by(Pclass) %>%
-  nest() %>%
-  mutate(M = map(data, function(dat){
-    dat2 <- dat %>% pivot_wider(names_from = Sex, values_from = freq)
-    M <- as.matrix(dat2[, -1])
-    row.names(M) <- dat2$Survived 
-    return(M)
-  })) %>% 
+  group_by(Pclass) %>% nest() %>%
+  mutate(M = map(data, ~ .x %>% 
+      pivot_wider(names_from = Sex, values_from = freq) %>% 
+      .[, -1] %>% as.matrix())) %>% 
   mutate(pvalue = map_dbl(M, ~chisq.test(.x)$p.value)) %>%
   select(-data, -M)
