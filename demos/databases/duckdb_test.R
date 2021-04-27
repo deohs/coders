4a6e46b66de49286776693f928b4f459fce1d77d
Brian High <high@uw.edu>
2021-04-27 21:11:39 +0000

    Updated for dataset including 2019 data.

--- demos/databases/duckdb_test.R
+++ demos/databases/duckdb_test.R
@@ -2,18 +2,17 @@
 # Data source: https://www.cdc.gov/brfss/annual_data/annual_data.htm
 # For R code to download BRFSS, see: https://github.com/brianhigh/brfss
 #
-# Size of BRFSS "data.table" for years 2012-2018 is 16973.8 MB in memory.
-# Size of dataset is   342.8 MB as (compressed) "rds" file.
-# Size of dataset is  3179.6 MB as "sqlite" file.
-# Size of dataset is 17596.0 MB as "duckdb" file. (Large size due to indexing.)
-# Dataset contains 3247335 observations and 685 variables.
+# Size of BRFSS "data.table" for years 2012-2019 is 21076 MB in memory.
+# Size of dataset is   343 MB as (compressed) "rds" file.
+# Size of dataset is  3180 MB as "sqlite" file.
+# Size of dataset is 17567 MB as "duckdb" file. (Larger size due to indexing.)
+# Dataset contains 3665603 observations and 764 variables.
 #
 # Summary of results:
 #
-# - readRDS is much faster at reading than Sqlite but not faster than DuckDB.
+# - readRDS is much faster than Sqlite but not much faster than DuckDB.
 # - DuckDB is much faster than SQlite when reading entire tables.
-# - DuckDB is much faster than SQlite when running new queries.
-# - DuckDB is faster than SQlite for queries which have been run before.
+# - DuckDB is much faster than SQlite when running queries.
 
 # Load packages
 if (!require(pacman)) install.packages("pacman")
@@ -23,7 +22,7 @@ pacman::p_load(data.table, DBI, RSQLite, duckdb)
 system.time(brfss <- readRDS(file.path("data", "brfss_data.rds")))
 
 ##   user  system elapsed 
-## 51.987  14.104  66.076 
+## 67.032  10.699  77.705 
 
 # Create the SQLite database
 con <- dbConnect(RSQLite::SQLite(), "brfss_data.sqlite")
@@ -33,7 +32,7 @@ dbWriteTable(con, "brfss", brfss)
 system.time(brfss_data <- dbReadTable(con, "brfss"))
 
 ##    user  system elapsed 
-## 644.020  58.490 702.359 
+## 651.522  57.603 708.933 
 
 # Remove the object to free the memory
 rm("brfss_data")
@@ -53,14 +52,15 @@ ORDER BY IYEAR;
 system.time(res <- dbGetQuery(con, query))
 
 ##  user  system elapsed 
-## 2.719   5.393   8.109 
+## 2.673   3.796   6.467 
 
 # Running a second time will likely be faster due to the index (and caching).
 system.time(res <- dbGetQuery(con, query))
 
 ##  user  system elapsed 
-## 2.403   2.636   5.035
+## 2.478   2.615   5.092
 
+# Close the connection
 dbDisconnect(con)
 
 # Create the DuckDB database
@@ -74,7 +74,7 @@ dbListTables(con_duck)
 system.time(brfss_data <- dbReadTable(con_duck, "brfss"))
 
 ##    user  system elapsed 
-##  63.605 304.157 367.658 
+##  41.850  41.608  83.380 
 
 # Remove the object to free the memory
 rm("brfss_data")
@@ -83,12 +83,13 @@ rm("brfss_data")
 system.time(res <- dbGetQuery(con_duck, query))
 
 ##  user  system elapsed 
-## 0.161   0.188   0.349 
+## 0.152   0.000   0.153 
 
 # Running a second time may run faster due to caching.
 system.time(res <- dbGetQuery(con_duck, query))
 
 ##  user  system elapsed 
-## 0.140   0.000   0.141
+## 0.148   0.000   0.147
 
-dbDisconnect(con_duck)
+# Close the connection
+dbDisconnect(con_duck, shutdown=TRUE)
