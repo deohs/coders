9fd3233ce28861a3f411837f9329e9def495b1f6
Brian High <high@uw.edu>
2021-08-05 18:14:12 +0000

    Added example script.

--- demos/hpc/parallel_processing/mclapply_with_splitting.R
+++ demos/hpc/parallel_processing/mclapply_with_splitting.R
@@ -15,28 +15,28 @@ R <- 10000     # number of replicates
 # Define a function that will take some time to process
 
 # Calculate robust covariance foreach bootstrap using a single core
-f <- function(X.seq, ...) {
+rc <- function(X.seq) {
   data(Cars93, package = "MASS")
   n <- nrow(Cars93)
   
   sapply(X.seq, function(x) {
-    robustbase::covMcd(Cars93[sample(1:n, replace=TRUE),
+    covMcd(Cars93[sample(1:n, replace=TRUE),
       c("Price", "Horsepower")], cor = TRUE)$cor[1,2]
   })
 }
 
 # For comparison, time the operation using a single core first
-system.time(ci_boot_single <- lapply(1:R, f))
+system.time(rc_boot_single <- lapply(1:R, rc))
 
 ## 93 seconds elapsed time
 
 # Total replicates (R) is much greater than the number of workers
-system.time(ci_boot <- mclapply(1:R, f, mc.cores = workers))
+system.time(rc_boot <- mclapply(1:R, rc, mc.cores = workers))
 
 ## 12 seconds elapsed time (8x speed improvement)
 
 # Splitting replicates by number of workers will speed up processing
 X.split <- split(1:R, rep_len(1:workers, length(1:R)))
-system.time(ci_boot2 <- mclapply(X.split, f, mc.cores = workers))
+system.time(rc_boot2 <- mclapply(X.split, rc, mc.cores = workers))
 
 ## 10 seconds elapsed time (1.2x speed improvement)
